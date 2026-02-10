6fb101e093652f282fde7c252b2d8a4d45af3de5
ravneetk05 <ravneet20051105@gmail.com>
2026-02-10 14:53:58 +0530

    \

--- index.js
+++ index.js
@@ -171,4 +171,6 @@ app.get("/health", (req, res) => {
     official_email: OFFICIAL_EMAIL,
   });
 });
-export default app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
